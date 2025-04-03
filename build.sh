befdad33b53f9c971293da4bf373b2f7f5be98d2
Piotr <pplonski86@gmail.com>
2025-04-03 12:19:10 +0200

    set version 1.0.0

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ jlpm run build
 
 python -m build
 
-cp dist/variable_inspector-1.0.1-py3-none-any.whl ../studio/env_installer/extras/
+cp dist/variable_inspector-1.0.0-py3-none-any.whl ../studio/env_installer/extras/
