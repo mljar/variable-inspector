a0901e4cfcd51c7006a423c4b5f3598919420bee
Piotr <pplonski86@gmail.com>
2025-09-25 10:47:19 +0200

    more generic build script, remove version

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+[ -d dist ] && rm -f dist/*
+
 jlpm run build
 
 python -m build
 
-cp dist/variable_inspector-1.1.0-py3-none-any.whl ../studio/env_installer/extras/
+cp dist/variable_inspector-*-py3-none-any.whl ../studio/env_installer/extras/
